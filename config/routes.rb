789f223d82388acc3b8c26ec79b4a32ecde8b7a4
Tom Leonard <tvleonard@gmail.com>
2021-05-21 08:54:45 -0400

    Add a route resource for HomeController.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :admins
   resources :admins
   resources :events
+  resources :home, only: [ :index ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
