74e1ec5cebaa28b622124d301f3a1cd2e683948f
Tom Leonard <tvleonard@gmail.com>
2021-05-21 08:52:57 -0400

    Add dev env seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env.development? # Only run in development environment!
+  unless Admin.exists?(email: 'admin@the-deluxe-venue.com')
+    admin = Admin.create(email: 'admin@the-deluxe-venue.com', password: 'secret', password_confirmation: 'secret')
+  end
+
+  if Event.count.zero?
+    (-5..15).each_with_index do |days, index|
+      Event.create(performer: "Performer #{index + 1}", when: DateTime.now + days, content: "This is the event content for Performer #{index + 1}")
+    end
+  end
+end
